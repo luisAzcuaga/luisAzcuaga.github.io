c705c6fcccc2494b59fe13eb8a5a740f53092d9d
Luis Azcuaga <37643667+luisAzcuaga@users.noreply.github.com>
2026-01-31 18:09:30 -0600

    feat: add resume; add profile picture

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <script setup lang="ts"></script>
+<style src="./App.css" />
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
+  <div id="main-wrapper">
+    <div id="left-section">
+      <section id="text-section">
+        <article>
+          <h1 class="title">This is Luis Azcuaga</h1>
+          <p>Hello! I'm a software engineer with experience in Ruby on Rails, Node and other web technologies.</p>
+          <p>I started my programming journey in 2018, and since then, I've worked on various personal and professional
+            projects. I'm passionate about writing clean, scalable, and efficient code.</p>
+        </article>
+      </section>
+      <section id="anchors-section">
+        <a href="https://github.com/luisAzcuaga">
+          <font-awesome-icon icon="fa-brands fa-github-alt" />
+        </a>
+        <a href="https://www.linkedin.com/in/luis-azcuaga">
+          <font-awesome-icon icon="fa-brands fa-linkedin" />
+        </a>
+        <a href="https://sortfella.azkawa.dev">
+          <font-awesome-icon icon="fa-solid fa-music" />
+        </a>
+        <a href="https://mind.azkawa.dev">
+          <font-awesome-icon icon="fa-solid fa-water" />
+        </a>
+        <a href="https://turnips.azkawa.dev">
+          <font-awesome-icon icon="fa-solid fa-leaf" />
+        </a>
+      </section>
 
-<style scoped></style>
+    </div>
+    <section id="right-section">
+      <img src="./luis.jpg" alt="logo"/>
+    </section>
+
+  </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import icons and add them to the Library */
+import { faMusic, faWater, faLeaf } from '@fortawesome/free-solid-svg-icons'
+import { faGithubAlt, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+library.add(faGithubAlt, faLinkedin, faMusic, faWater, faLeaf)
 
-createApp(App).mount('#app')
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
